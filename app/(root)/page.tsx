df09b83ea25bb0086b7df2d6f98521c894c2a195
iamVictorWong <wtcctwwtc@gmail.com>
2024-12-06 18:28:21 -0500

    add user interface components - bank account balance, header box, animated counter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
+const LoggedIn = {firstName: "Victor"}
+
     return (
-        <div>
-        <h1>Home</h1>
-        </div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox 
+                        type="greeting"
+                        title="Welcome to VW Bank"
+                        user= {LoggedIn?.firstName || "Guest"}
+                        subtext="Trust Funding Banking Platform"
+                    />
+                    <TotalBalanceBox
+                        account={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                    />
+                </header>
+            </div>
+        </section>
     );
 }
 
